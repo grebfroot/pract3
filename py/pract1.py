b22deaaef52494cf9821e379943f337eac8ed2b7
grebfroot <98997758+grebfroot@users.noreply.github.com>
2023-09-22 15:47:21 +0600

    Update pract1.py

--- py/pract1.py
+++ py/pract1.py
@@ -2,23 +2,30 @@ a1=float(input("Введите первое число: "))
 operation=input("Введите операцию (+, -, *, /): ")
 a2=float(input("Введите второе число: "))
 
-if operation not in ('+','-','*','/'):
-    print("Недопустимая операция.")
+while operation!='1':
+    if operation not in ('+','-','*','/'):
+        print("Недопустимая операция.")
     
-else:
-    
-    if operation=='/' and a2==0:
-        print("Нельзя делить на ноль.")
-        
     else:
+    
+        if operation=='/' and a2==0:
+            print("Нельзя делить на ноль.")
         
-        if operation=='+':
-            rezult=a1+a2
-        elif operation=='-':
-            rezult=a1-a2
-        elif operation=='*':
-            rezult=a1*a2
-        elif operation=='/':
-            rezult=a1/a2
+        else:
+        
+            if operation=='+':
+                rezult=a1+a2
+            elif operation=='-':
+                rezult=a1-a2
+            elif operation=='*':
+                rezult=a1*a2
+            elif operation=='/':
+                rezult=a1/a2
 
-        print("Результат:", rezult)
+            print("Результат:", rezult)
+    operation=input("Чтобы завершить вычисление нажмите 1. Чтобы продолжить - снова выберите операцию (+, -, *, /):")
+    if operation!='1':
+        a1=rezult
+        a2=float(input("Введите второе число: "))
+else:
+    print("Вычисление завершено.")
